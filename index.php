bb8dfc7b96de984b1d75283fc5e32d8d13c2831e
ed-cd <metatron.addict@gmail.com>
2015-06-06 19:45:45 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="css/style.css?ver=<?php echo filemtime('css/style.css');?>"
+	type="text/css" rel="stylesheet" />
+<meta charset="utf-8">
+<meta name="description" content="FreeBSD">
+<title>FreeBSD</title>
+</head>
+<body>
+	<div class="main">
+		<div class="bodyBox">
+
+			<div class="head">
+				<div id="headerLeftPaddingDiv">&nbsp;</div>
+				<div id="headerRightContent">
+					<div id="headerNav">
+						Text Size: <a href="bob.ca">Normal</a> / <a href="bob.ca">Large</a>
+						| <a href="bob.ca">Donate</a> | <a href="bob.ca">Contact</a>
+					</div>
+					<br />
+					<div id="headerSearch">Search</div>
+				</div>
+				<div id="headerRightPaddingDiv">&nbsp;</div>
+			</div>
+
+			<div id="topMenu">
+				<ul>
+					<li>Home</li>
+					<li>|</li>
+					<li>About</li>
+					<li>|</li>
+					<li>Get FreeBSD</li>
+					<li>|</li>
+					<li>Documentation</li>
+					<li>|</li>
+					<li>Community</li>
+					<li>|</li>
+					<li>Developers</li>
+					<li>|</li>
+					<li>Support</li>
+					<li>|</li>
+					<li>Foundation</li>
+				</ul>
+
+			</div>
+
+			<div class="bodyBox">
+				<div class="leftNavigation">
+					<ul class="navigationLinks">
+						<li><a href="invalidlink.com">Developers/</a></li>
+						<li><a href="invalidlink.com">Developer's Handbook/</a></li>
+						<li><a href="invalidlink.com">Porter's Handbook</a></li>
+						<li><a href="invalidlink.com">Source code repositories</a></li>
+						<li><a href="invalidlink.com">Release Engineering/</a></li>
+						<li><a href="invalidlink.com">Platforms/</a></li>
+						<li><a href="invalidlink.com">Project Ideas</a></li>
+						<li><a href="invalidlink.com">Contributing</a></li>
+					</ul>
+				</div>
+
+				<div class="rightContent">
+					<div>
+						<h1>FreeBSD/ppc Project</h1>
+						<br />
+						<h2>Introduction</h2>
+
+						<p>The FreeBSD/ppc project pages contain information about the
+							FreeBSD port to the PowerPC&#174; architecture. As with the port
+							itself, these pages are still a work in progress.</p>
+
+						<h2>Table Of Contents</h2>
+
+
+						<ul>
+							<li><a href="invalidlink.com">Status/</a></li>
+							<li><a href="invalidlink.com">Latest News/</a></li>
+							<li><a href="invalidlink.com">Port FAQs</a></li>
+							<li><a href="invalidlink.com">Supported Hardware</a></li>
+							<li><a href="invalidlink.com">FreeBSD/ppc mailing list</a></li>
+							<li><a href="invalidlink.com">Known Issues</a></li>
+							<li><a href="invalidlink.com">Documentation, White Papers,
+									Utilities</a></li>
+							<li><a href="invalidlink.com">Other links of interest</a></li>
+						</ul>
+
+						<h2>Status</h2>
+
+						<p>
+							The FreeBSD/ppc port is still a <a href="invalidlink.com">Tier 2</a>
+							platform. That means it is not being fully supported by our
+							security officer, release engineers and toolchain maintainers.
+						</p>
+
+						<hr />
+
+						<h2>Latest News</h2>
+
+						<ul style="font-size: 90%">
+							<li><b>03 April, 2009:</b> Support for the PowerPC G5 has been
+								committed to SVN.</li>
+							<li><b>03 March, 2008:</b> Support for the
+								Freescale&#174;PowerQUICC MPC85XX family system-on-chip
+								development boards has been comitted into CVS.</li>
+							<li><b>27 February, 2008:</b> FreeBSD 7.0 is the first release to
+								oficially support the FreeBSD/ppc port.</li>
+						</ul>
+
+						<hr />
+
+						<h2>Frequently Asked Questions</h2>
+
+						<ul style="list-style: decimal">
+							<li><a href="invalidlink.com">How can i install FreeBSD/ppc</a></li>
+							<li><a href="invalidlink.com">How to use ports on the
+									FreeBSD/ppc?</a></li>
+							<li><a href="invalidlink.com">Who should I contact?</a></li>
+						</ul>
+
+						<h2>How can I install FreeBSD/ppc?</h2>
+
+						<p>
+							ISO images of FreeBSD 8.2 suitable for New-World Macs are
+							available for download, for details on how to obtain these see
+							the <a href="invalidlink.com">release announcement.</a> please
+							follow the instructions given <a href="invalidlink.com">here.</a>
+						</p>
+
+						<h2>How to use ports on FreeBSD/ppc?</h2>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
