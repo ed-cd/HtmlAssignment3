36ce973d3f8dafa6d4045c993f5ca680f21c668e
ed-cd <metatron.addict@gmail.com>
2015-06-07 23:58:18 -0700

    Final commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			<div id="headerRightContent">CALL TOLL FREE 1-800-772-5823</div>
 		</div>
 		<div id="topMenu">
-			<ul>
+									<ul>
 				<li id="topMenuLeftPaddingDiv" class="button">&nbsp;</li><!--
 				--><li>Home</li><!--
 				--><li>Market Segments</li><!--
@@ -26,80 +26,93 @@
 				--><li>Market Area</li><!--
 				--><li>Market Information</li>
 			</ul>
-			<div id="unknownPaddingdiv"><div id="whiteBox">&nbsp;</div></div>
+			<div id="unknownPaddingdiv">
+				<div id="whiteBox">&nbsp;</div>
+			</div>
 		</div>
 
 		<div class="bodyBox">
-			<img id="premisesPic" src="screen4-images/main_pic.png" alt="premises"/>
+			<img id="premisesPic" src="screen4-images/main_pic.png"
+				alt="premises" />
 
 			<div class="infoBox">
-					<h1>ABOUT PPC LUBRICANTS</h1>
-					<br />
-					<h2>Introduction</h2>
+				<h1>ABOUT PPC LUBRICANTS</h1>
+				<div class="whiteOnGrey">
+					<h2>A Heritage of specializing in Lubricants</h2>
 
-					<p>The FreeBSD/ppc project pages contain information about the
-						FreeBSD port to the PowerPC&#174; architecture. As with the port
-						itself, these pages are still a work in progress.</p>
-
-					<h2>Table Of Contents</h2>
-
-
-					<ul>
-						<li><a href="invalidlink.com">Status/</a></li>
-						<li><a href="invalidlink.com">Latest News/</a></li>
-						<li><a href="invalidlink.com">Port FAQs</a></li>
-						<li><a href="invalidlink.com">Supported Hardware</a></li>
-						<li><a href="invalidlink.com">FreeBSD/ppc mailing list</a></li>
-						<li><a href="invalidlink.com">Known Issues</a></li>
-						<li><a href="invalidlink.com">Documentation, White Papers,
-								Utilities</a></li>
-						<li><a href="invalidlink.com">Other links of interest</a></li>
-					</ul>
-
-					<h2>Status</h2>
-
-					<p>
-						The FreeBSD/ppc port is still a <a href="invalidlink.com">Tier 2</a>
-						platform. That means it is not being fully supported by our
-						security officer, release engineers and toolchain maintainers.
-					</p>
-
-					<hr />
-
-					<h2>Latest News</h2>
-
-					<ul style="font-size: 90%">
-						<li><b>03 April, 2009:</b> Support for the PowerPC G5 has been
-							committed to SVN.</li>
-						<li><b>03 March, 2008:</b> Support for the
-							Freescale&#174;PowerQUICC MPC85XX family system-on-chip
-							development boards has been comitted into CVS.</li>
-						<li><b>27 February, 2008:</b> FreeBSD 7.0 is the first release to
-							oficially support the FreeBSD/ppc port.</li>
-					</ul>
-
-					<hr />
-
-					<h2>Frequently Asked Questions</h2>
-
-					<ul style="list-style: decimal">
-						<li><a href="invalidlink.com">How can i install FreeBSD/ppc</a></li>
-						<li><a href="invalidlink.com">How to use ports on the FreeBSD/ppc?</a></li>
-						<li><a href="invalidlink.com">Who should I contact?</a></li>
-					</ul>
-
-					<h2>How can I install FreeBSD/ppc?</h2>
-
-					<p>
-						ISO images of FreeBSD 8.2 suitable for New-World Macs are
-						available for download, for details on how to obtain these see the
-						<a href="invalidlink.com">release announcement.</a> please follow
-						the instructions given <a href="invalidlink.com">here.</a>
-					</p>
-
-					<h2>How to use ports on FreeBSD/ppc?</h2>
+					<p>Lorem ipsum dolor sit amet, eos ad malis commodo interesset. Mea
+						ne audire delectus, id unum voluptatum eos, ea tale nonumy mei. Te
+						ferri viris ceteros usu, pri at laoreet vituperata, cu rebum
+						lobortis pericula eum. Vim veri reque nobis in, sit possim
+						apeirian ea, has in latine expetenda.</p>
+					<p>No usu choro veniam, te est elit dolore. Vis zril audire ex, ne
+						alterum elaboraret ius. Pri ut consul verear, et prima neglegentur
+						vix, tamquam democritum cu eos. Consul numquam no vim, erant
+						graecis constituto pro te. Alii regione ei quo, essent nusquam
+						constituam et quo, ius scaevola recteque laboramus eu. Vel integre
+						petentium qualisque at, eu quo rebum nobis meliore. At nam posse
+						platonem.</p>
+				</div>
 			</div>
+
+			<div id="ppcDiv">
+				<img alt="logo" src="screen4-images/logo.png" />PPC now has Bulk
+				Delivery/Quirt Fill capabilities for DEF
+			</div>
+
+			<div class="infoBox small">
+				<h1>Automotive</h1>
+
+				<img alt="automotive" src="screen4-images/automotive.png" />
+				<div class=blackOnWhite>
+					<p>Lorem ipsum dolor sit amet, eos ad malis commodo interesset. Mea
+						ne audire delectus, id unum voluptatum eos, ea tale nonumy mei.</p>
+					<img class="readMore" alt="read more"
+						src="screen4-images/read_more.png" />
+				</div>
+			</div>
+			<div class="infoBox small">
+				<h1>Comercial</h1>
+
+				<img alt="automotive" src="screen4-images/automotive.png" />
+				<div class=blackOnWhite>
+					<p>Lorem ipsum dolor sit amet, eos ad malis commodo interesset. Mea
+						ne audire delectus, id unum voluptatum eos, ea tale nonumy mei.</p>
+					<img class="readMore" alt="read more"
+						src="screen4-images/read_more.png" />
+				</div>
+			</div>
+			<div class="infoBox small">
+				<h1>Industrial</h1>
+
+				<img alt="automotive" src="screen4-images/automotive.png" />
+				<div class=blackOnWhite>
+					<p>Lorem ipsum dolor sit amet, eos ad malis commodo interesset. Mea
+						ne audire delectus, id unum voluptatum eos, ea tale nonumy mei.</p>
+					<img class="readMore" alt="read more"
+						src="screen4-images/read_more.png" />
+				</div>
+			</div>
+						<div class="infoBox small">
+				<h1>Diesel Exhaust Fluid</h1>
+
+				<img alt="automotive" src="screen4-images/automotive.png" />
+				<div class=blackOnWhite>
+					<p>Lorem ipsum dolor sit amet, eos ad malis commodo interesset. Mea
+						ne audire delectus, id unum voluptatum eos, ea tale nonumy mei.</p>
+					<img class="readMore" alt="read more"
+						src="screen4-images/read_more.png" />
+				</div>
+			</div>
+
+
 		</div>
+
+
+
+	</div>
+
+	</div>
 	</div>
 </body>
 </html>
