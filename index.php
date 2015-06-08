eaf8adf0b12cb9a548e06f4f74f94b6edeb57e26
ed-cd <metatron.addict@gmail.com>
2015-06-07 18:12:39 -0700

    Top part complete

--- index.php
+++ index.php
@@ -9,129 +9,95 @@
 <title>FreeBSD</title>
 </head>
 <body>
-<p></p>
+	<p></p>
 	<div class="main">
+		<div class="head">
+			<img alt="logo" src="screen4-images/logo.png" />
+			<div id="headerRightContent">CALL TOLL FREE 1-800-772-5823</div>
+		</div>
+		<div id="topMenu">
+			<ul>
+				<li id="topMenuLeftPaddingDiv" class="button">&nbsp;</li><!--
+				--><li>Home</li><!--
+				--><li>Market Segments</li><!--
+				--><li>Services</li><!--
+				--><li>Products</li><!--
+				--><li>Promotions</li><!--
+				--><li>Market Area</li><!--
+				--><li>Market Information</li>
+			</ul>
+			<div id="unknownPaddingdiv"><div id="whiteBox">&nbsp;</div></div>
+		</div>
+
 		<div class="bodyBox">
+			<img id="premisesPic" src="screen4-images/main_pic.png" alt="premises"/>
 
-			<div class="head">
-				<div id="headerLeftPaddingDiv">&nbsp;</div>
-				<div id="headerRightContent">
-					<div id="headerNav">
-						Text Size: <a href="bob.ca">Normal</a> / <a href="bob.ca">Large</a>
-						| <a href="bob.ca">Donate</a> | <a href="bob.ca">Contact</a>
-					</div>
+			<div class="infoBox">
+					<h1>ABOUT PPC LUBRICANTS</h1>
 					<br />
-					<div id="headerSearch">Search</div>
-				</div>
-				<div id="headerRightPaddingDiv">&nbsp;</div>
-			</div>
+					<h2>Introduction</h2>
 
-			<div id="topMenu">
-				<ul>
-					<li>Home</li>
-					<li>|</li>
-					<li>About</li>
-					<li>|</li>
-					<li>Get FreeBSD</li>
-					<li>|</li>
-					<li>Documentation</li>
-					<li>|</li>
-					<li>Community</li>
-					<li>|</li>
-					<li>Developers</li>
-					<li>|</li>
-					<li>Support</li>
-					<li>|</li>
-					<li>Foundation</li>
-				</ul>
+					<p>The FreeBSD/ppc project pages contain information about the
+						FreeBSD port to the PowerPC&#174; architecture. As with the port
+						itself, these pages are still a work in progress.</p>
 
-			</div>
+					<h2>Table Of Contents</h2>
 
-			<div class="bodyBox">
-				<div class="leftNavigation">
-					<ul class="navigationLinks">
-						<li><a href="invalidlink.com">Developers/</a></li>
-						<li><a href="invalidlink.com">Developer's Handbook/</a></li>
-						<li><a href="invalidlink.com">Porter's Handbook</a></li>
-						<li><a href="invalidlink.com">Source code repositories</a></li>
-						<li><a href="invalidlink.com">Release Engineering/</a></li>
-						<li><a href="invalidlink.com">Platforms/</a></li>
-						<li><a href="invalidlink.com">Project Ideas</a></li>
-						<li><a href="invalidlink.com">Contributing</a></li>
+
+					<ul>
+						<li><a href="invalidlink.com">Status/</a></li>
+						<li><a href="invalidlink.com">Latest News/</a></li>
+						<li><a href="invalidlink.com">Port FAQs</a></li>
+						<li><a href="invalidlink.com">Supported Hardware</a></li>
+						<li><a href="invalidlink.com">FreeBSD/ppc mailing list</a></li>
+						<li><a href="invalidlink.com">Known Issues</a></li>
+						<li><a href="invalidlink.com">Documentation, White Papers,
+								Utilities</a></li>
+						<li><a href="invalidlink.com">Other links of interest</a></li>
 					</ul>
-				</div>
 
-				<div class="rightContent">
-					<div>
-						<h1>FreeBSD/ppc Project</h1>
-						<br />
-						<h2>Introduction</h2>
+					<h2>Status</h2>
 
-						<p>The FreeBSD/ppc project pages contain information about the
-							FreeBSD port to the PowerPC&#174; architecture. As with the port
-							itself, these pages are still a work in progress.</p>
+					<p>
+						The FreeBSD/ppc port is still a <a href="invalidlink.com">Tier 2</a>
+						platform. That means it is not being fully supported by our
+						security officer, release engineers and toolchain maintainers.
+					</p>
 
-						<h2>Table Of Contents</h2>
+					<hr />
 
+					<h2>Latest News</h2>
 
-						<ul>
-							<li><a href="invalidlink.com">Status/</a></li>
-							<li><a href="invalidlink.com">Latest News/</a></li>
-							<li><a href="invalidlink.com">Port FAQs</a></li>
-							<li><a href="invalidlink.com">Supported Hardware</a></li>
-							<li><a href="invalidlink.com">FreeBSD/ppc mailing list</a></li>
-							<li><a href="invalidlink.com">Known Issues</a></li>
-							<li><a href="invalidlink.com">Documentation, White Papers,
-									Utilities</a></li>
-							<li><a href="invalidlink.com">Other links of interest</a></li>
-						</ul>
+					<ul style="font-size: 90%">
+						<li><b>03 April, 2009:</b> Support for the PowerPC G5 has been
+							committed to SVN.</li>
+						<li><b>03 March, 2008:</b> Support for the
+							Freescale&#174;PowerQUICC MPC85XX family system-on-chip
+							development boards has been comitted into CVS.</li>
+						<li><b>27 February, 2008:</b> FreeBSD 7.0 is the first release to
+							oficially support the FreeBSD/ppc port.</li>
+					</ul>
 
-						<h2>Status</h2>
+					<hr />
 
-						<p>
-							The FreeBSD/ppc port is still a <a href="invalidlink.com">Tier 2</a>
-							platform. That means it is not being fully supported by our
-							security officer, release engineers and toolchain maintainers.
-						</p>
+					<h2>Frequently Asked Questions</h2>
 
-						<hr />
+					<ul style="list-style: decimal">
+						<li><a href="invalidlink.com">How can i install FreeBSD/ppc</a></li>
+						<li><a href="invalidlink.com">How to use ports on the FreeBSD/ppc?</a></li>
+						<li><a href="invalidlink.com">Who should I contact?</a></li>
+					</ul>
 
-						<h2>Latest News</h2>
+					<h2>How can I install FreeBSD/ppc?</h2>
 
-						<ul style="font-size: 90%">
-							<li><b>03 April, 2009:</b> Support for the PowerPC G5 has been
-								committed to SVN.</li>
-							<li><b>03 March, 2008:</b> Support for the
-								Freescale&#174;PowerQUICC MPC85XX family system-on-chip
-								development boards has been comitted into CVS.</li>
-							<li><b>27 February, 2008:</b> FreeBSD 7.0 is the first release to
-								oficially support the FreeBSD/ppc port.</li>
-						</ul>
+					<p>
+						ISO images of FreeBSD 8.2 suitable for New-World Macs are
+						available for download, for details on how to obtain these see the
+						<a href="invalidlink.com">release announcement.</a> please follow
+						the instructions given <a href="invalidlink.com">here.</a>
+					</p>
 
-						<hr />
-
-						<h2>Frequently Asked Questions</h2>
-
-						<ul style="list-style: decimal">
-							<li><a href="invalidlink.com">How can i install FreeBSD/ppc</a></li>
-							<li><a href="invalidlink.com">How to use ports on the
-									FreeBSD/ppc?</a></li>
-							<li><a href="invalidlink.com">Who should I contact?</a></li>
-						</ul>
-
-						<h2>How can I install FreeBSD/ppc?</h2>
-
-						<p>
-							ISO images of FreeBSD 8.2 suitable for New-World Macs are
-							available for download, for details on how to obtain these see
-							the <a href="invalidlink.com">release announcement.</a> please
-							follow the instructions given <a href="invalidlink.com">here.</a>
-						</p>
-
-						<h2>How to use ports on FreeBSD/ppc?</h2>
-
-					</div>
-				</div>
+					<h2>How to use ports on FreeBSD/ppc?</h2>
 			</div>
 		</div>
 	</div>
